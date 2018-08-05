0d94430424d6cce45f5809d1cbad05eae88e6e38
thomastieyi <38903753+thomastieyi@users.noreply.github.com>
2018-08-06 00:09:09 +0800

    Add files via upload

--- Unt2.js
+++ Unt2.js
@@ -7,7 +7,7 @@ var app = express();
 app.get('/', function (req, res) {
    console.log("主页 GET 请求");
       console.log(req.originalUrl);
-   res.send();
+   res.send( __dirname + "\\" + "index.htm");
 })
  
  
@@ -36,7 +36,7 @@ app.get('/', function (req, res) {
 //})
  
  
-var server = app.listen(8080, function () {
+var server = app.listen(8081, function () {
  
   var host = server.address().address
   var port = server.address().port
